27ce1cdbdd302e5b141c68833c213ee3983d50a0
ido <ido@dooble.co.il>
2018-06-12 15:37:43 +0300

    "bump to 1.0.2"

--- index.js
+++ index.js
@@ -5,55 +5,63 @@ module.exports = {
         'core.plugin.type',
         'core.loader.types',
     ],
-    types: [{
-        name: 'module',
-        schema: [
-            {
-                key: 'name',
-                type: 'string',
-                description: 'The name of the module.',
-                required: true
-            },{
-                key: 'dependencies',
-                type: 'array',
-                ofType: 'module',
-                description: `locally defined dependencies.`,
-                items: []
-            },{
-                key: 'get',
-                type: 'function',
-                source: '() => {  }',
-                compiled: 'function(){  }'
-            }
-        ],
-        build(definition, done) {
+    types: [
+        {
+            name: 'dependencies',
+            extends: 'array',
+            schema: [{
+                key: 'itemsType',
+                
+            }]
+        },{
+            name: 'module',
+            identifier: 'name',
+            schema: [
+                {
+                    key: 'name',
+                    type: 'string',
+                    description: 'The name of the module.',
+                    isRequired: true
+                },{
+                    key: 'dependencies',
+                    type: 'array',
+                    ofType: 'module',
+                    description: `locally defined dependencies.`,
+                    items: []
+                },{
+                    key: 'get',
+                    type: 'function',
+                    source: '() => {  }',
+                    compiled: 'function(){  }'
+                }
+            ],
+            build(definition, done) {
 
-            var core = this;
-            var {
-                name,
-                dependencies,
-                value,
-                get,
-            } = definition;
+                var core = this;
+                var {
+                    name,
+                    dependencies,
+                    value,
+                    get,
+                } = definition;
 
-            this.definitions[definition.name] = definition;
-            
-            if ('value' in definition) {
-                core.injector.load(name, value);
-                done && done(value);
-                return value;
-            } else if(!get){
-                throw new Error(`core.modules - a module must define either a 'value' property or a 'get' function.`)
-            } else {
-                return core.injector.load(name, dependencies || [], function(){
-                    value = get.apply(core, arguments);
-                    core.modules[name] = value;
+                if ('value' in definition) {
+                    core.injector.load(name, value);
                     done && done(value);
                     return value;
-                });
+                } else if(!get){
+                    throw new Error(`core.modules - a module must define either a 'value' property or a 'get' function.`)
+                } else {
+                    return core.injector.load(name, dependencies || [], function(){
+                        value = get.apply(core, arguments);
+                        core.modules[name] = value;
+                        done && done(value);
+                        return value;
+                    });
+                }
             }
         }
-    }],
+    ],
     extend: {
         modules: {},
         Module(name, dependencies, get, done) {
@@ -61,7 +69,15 @@ module.exports = {
                 return name.map(this.Module)
             }
             var definition = this.getDefinitionObject(name, dependencies, get, 'module', done);
-            return this.build(definition, definition.done);
+            
+            var source = this.type.toSource({
+                id: definition.name,
+                key: definition.name,
+                type: 'module',
+                description: definition.description || '',
+              }, definition);
+        
+            return this.build(source, definition.done);
         },
     }
 };
